3468b9ec7706741a50be2512e462a73498c11fb6
Houshic B <88103192+Houshic19@users.noreply.github.com>
2023-03-29 21:27:14 +0530

    Add files via upload

---
+++ Day 4 - Jump Game - II .java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+
+        int answer = 0, n = nums.length;
+        int curr = 0, far = 0;
+        
+        for (int i = 0; i < n - 1; ++i) {
+
+            far = Math.max(far, i + nums[i]);
+            
+            if (i == curr) {
+                answer++;
+                curr = far;
+            }
+        }
+        
+        return answer;
+    }
+}
